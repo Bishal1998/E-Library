ec29825e52451da41e1d62e3acc89a299adfde7c
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-04-29 18:47:20 +0545

    express setup and server start

--- apis/src/app.ts
+++ apis/src/app.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+// Routes
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "Welcome to E-Library!" });
+});
+
 export default app;
