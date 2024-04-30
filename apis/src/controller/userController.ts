1bbdc124dbe4b276a9ee6c2a106d5f6f8ccd4949
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-04-30 11:17:37 +0545

    error handling

--- apis/src/controller/userController.ts
+++ apis/src/controller/userController.ts
@@ -15,27 +15,31 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
 
-  // check user exists
-  const user = await User.findOne({ email });
-  if (user) {
-    const error = createHttpError(400, "User already exists");
-    return next(error);
+  try {
+    // check user exists
+    const user = await User.findOne({ email });
+    if (user) {
+      const error = createHttpError(400, "User already exists with this email");
+      return next(error);
+    }
+
+    // password hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // save data in database
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    //token generation
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    // response
+    res.json({ message: "User Registered Successfully", accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Error creating user"));
   }
-
-  // password hashing
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  // save data in database
-  const newUser = new User({ name, email, password: hashedPassword });
-  await newUser.save();
-
-  //token generation
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-  });
-
-  // response
-  res.json({ message: "User Registered Successfully", accessToken: token });
 };
 
 export { createUser };
