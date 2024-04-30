bb24079001deef4744dd238adc0a7e71bd715164
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-04-30 10:29:15 +0545

    controller user validate

--- apis/src/app.ts
+++ apis/src/app.ts
@@ -3,6 +3,7 @@ import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./routes/userRoutes";
 
 const app = express();
+app.use(express.json());
 
 // Routes
 
--- apis/src/controller/userController.ts
+++ apis/src/controller/userController.ts
@@ -1,6 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  //get data
+  const { name, email, password } = req.body;
+
+  // validate data
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
   res.json({ message: "User Registered Successfully" });
 };
 
