f43e616c0cc0d40d192e0ff5d7627ebcbbe0618f
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-04-30 12:58:36 +0545

    check for file uploads

--- apis/src/controller/bookController.ts
+++ apis/src/controller/bookController.ts
@@ -3,6 +3,7 @@ import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    console.log("files", req.files);
     res.json({ message: "Book created" });
   } catch (error) {
     return next(createHttpError(500, "Unable to Create Book"));
