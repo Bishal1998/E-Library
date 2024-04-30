0bc79fbb37d12187e24157161f4059809938ee28
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-04-30 12:21:15 +0545

    book schema added

--- apis/src/index.type.ts
+++ apis/src/index.type.ts
@@ -4,3 +4,15 @@ export interface User {
   email: string;
   password: string;
 }
+
+export interface Book {
+  _id: string;
+  title: string;
+  author: User;
+  genre: string;
+  coverImage: string;
+  file: string;
+  createdAt: Date;
+  updatedAt: Date;
+  description: string;
+}
---
+++ apis/src/models/bookModel.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import { Book } from "../index.type";
+
+const bookSchema = new mongoose.Schema<Book>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    file: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model<Book>("Book", bookSchema);
+
+export default Book;
