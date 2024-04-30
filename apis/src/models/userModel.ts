848e0977b89130fcf49085eab4c30487949143d1
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-04-30 10:35:34 +0545

    user model created

---
+++ apis/src/models/userModel.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { User } from "../index.type";
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: [true, "E-mail is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model<User>("User", userSchema);
+export default User;
